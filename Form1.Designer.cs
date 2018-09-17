206d64e388d4206fa58034491a7f8c706500be3b
acarney9 <acmustang920@gmail.com>
2018-09-16 20:57:15 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace StudentAvgTwoDimArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.showScoresButton = new System.Windows.Forms.Button();
+            this.averageButton = new System.Windows.Forms.Button();
+            this.testAverageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.displayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.displayLabel.Location = new System.Drawing.Point(105, 60);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(389, 201);
+            this.displayLabel.TabIndex = 0;
+            // 
+            // showScoresButton
+            // 
+            this.showScoresButton.Location = new System.Drawing.Point(58, 312);
+            this.showScoresButton.Name = "showScoresButton";
+            this.showScoresButton.Size = new System.Drawing.Size(137, 53);
+            this.showScoresButton.TabIndex = 1;
+            this.showScoresButton.Text = "Show Scores";
+            this.showScoresButton.UseVisualStyleBackColor = true;
+            this.showScoresButton.Click += new System.EventHandler(this.showScoresButton_Click);
+            // 
+            // averageButton
+            // 
+            this.averageButton.Location = new System.Drawing.Point(229, 312);
+            this.averageButton.Name = "averageButton";
+            this.averageButton.Size = new System.Drawing.Size(137, 53);
+            this.averageButton.TabIndex = 2;
+            this.averageButton.Text = "Show Average";
+            this.averageButton.UseVisualStyleBackColor = true;
+            this.averageButton.Click += new System.EventHandler(this.averageButton_Click);
+            // 
+            // testAverageButton
+            // 
+            this.testAverageButton.Location = new System.Drawing.Point(399, 312);
+            this.testAverageButton.Name = "testAverageButton";
+            this.testAverageButton.Size = new System.Drawing.Size(137, 53);
+            this.testAverageButton.TabIndex = 3;
+            this.testAverageButton.Text = "Test Averages";
+            this.testAverageButton.UseVisualStyleBackColor = true;
+            this.testAverageButton.Click += new System.EventHandler(this.testAverageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 432);
+            this.Controls.Add(this.testAverageButton);
+            this.Controls.Add(this.averageButton);
+            this.Controls.Add(this.showScoresButton);
+            this.Controls.Add(this.displayLabel);
+            this.Name = "Form1";
+            this.Text = "Student Average";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.Button showScoresButton;
+        private System.Windows.Forms.Button averageButton;
+        private System.Windows.Forms.Button testAverageButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Alex Carney
+//11/16/2017
+namespace StudentAvgTwoDimArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showScoresButton_Click(object sender, EventArgs e)
+        {
+            //Create a 2 dim array to display data
+
+            double[,] scores = { { 78, 94, 67},
+                                 { 97, 93, 90},
+                                 { 65, 67, 71},
+                                 { 84, 85, 87}
+                                };
+
+            // pass the array to ShowData method
+            ShowData(scores);
+        }
+
+
+        private void ShowData(double[,] theScores)
+        {
+            string output = "";
+
+            for (int row = 0; row < theScores.GetLength(0); row++)
+            {
+                for (int col = 0; col < theScores.GetLength(1); col++)
+                {
+                    // traverse the array and concatenate the number
+
+                    output = output + theScores[row, col] + " ";
+                }
+
+                output += "\n";
+
+                displayLabel.Text = output;
+            }
+        }
+
+        private void averageButton_Click(object sender, EventArgs e)
+        {
+            double[,] scores = { { 78, 94, 67},
+                                 { 97, 93, 90},
+                                 { 65, 67, 71},
+                                 { 84, 85, 87}
+                                };
+
+            ShowStudentAverages(scores);
+
+        }
+
+
+        private void ShowStudentAverages(double[,] theScores)
+        {
+            string output = "";
+            double studentSum = 0;
+            double studentAvg = 0;
+
+            for (int row = 0; row < theScores.GetLength(0); row++)
+            {
+                for (int col = 0; col < theScores.GetLength(1); col++)
+                {
+                    studentSum += theScores[row, col];
+
+                }
+
+                studentAvg = studentSum / theScores.GetLength(1);
+
+                output += "Student " + (row + 1) + " Avg: " + studentAvg.ToString("n2") + "\n";
+
+                studentSum = 0;
+
+
+            }
+
+            displayLabel.Text = output;
+        }
+
+        private void testAverageButton_Click(object sender, EventArgs e)
+        {
+
+            
+                double[,] scores = { { 78, 94, 67},
+                                     { 97, 93, 90},
+                                     { 65, 67, 71},
+                                     { 84, 85, 87}
+                                    };
+
+                ShowTestAverages(scores);
+
+        }
+
+
+        private void ShowTestAverages(double[,] theScores)
+        {
+                string output = "";
+                double testSum = 0;
+                double testAvg = 0;
+
+                for (int col = 0; col < theScores.GetLength(1); col++)
+                {
+                    for (int row = 0; row < theScores.GetLength(0); row++)
+                    {
+                        testSum += theScores[row, col];
+
+                    }
+
+                    testAvg = testSum / theScores.GetLength(0);
+
+                    output += "Test " + (col + 1) + " Avg: " + testAvg.ToString("n2") + "\n";
+
+                    testSum = 0;
+
+
+                }
+
+                displayLabel.Text = output;
+
+        }
+
+    }
+}
+
+    
+
